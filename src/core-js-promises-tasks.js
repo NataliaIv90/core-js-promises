3e406f81da493ed63aacd1e3297a935107a5e4ba
Natalia Ivantsova <ivantsovana@gmail.com>
2024-04-02 11:48:54 +0300

    feat: add promise.all solution

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -111,8 +111,15 @@ function getFirstPromiseResult(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)] => Promise rejected with 2
  */
-function getAllOrNothing(/* promises */) {
-  throw new Error('Not implemented');
+function getAllOrNothing(promises) {
+  return Promise.all(
+    promises.map((promise) =>
+      promise.then(
+        (value) => Promise.resolve(value),
+        (error) => Promise.reject(error)
+      )
+    )
+  );
 }
 
 /**
